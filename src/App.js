c0ec817d8e691c3e99d514007344a0580e45eb2a
Adler11th <mgoftest@gmail.com>
2018-03-09 02:05:13 -0800

    search bar component styles needed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from "./SearchBar"
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      eventCategories: []
+    }
+  }
+  componentDidMount(){
+    fetch("https://api.meetup.com/2/categories?&sign=true&photo-host=public&page=20&key=436d386b764018255e52695046494f59")
+    .then(results=>{
+      if(results.ok){
+      results.json();
+      }else{
+        console.log("didn't get results...");
+      }
+    }).then(data => {
+      console.log(data);
+      // this.setState({eventCategories: data});
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <SearchBar categories = {this.state.eventCategories}/>
     );
   }
 }
