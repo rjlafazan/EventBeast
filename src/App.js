3238e7f7a9d381f2b5879ccdc0d257f8fdc20e24
Adler11th <mgoftest@gmail.com>
2018-03-10 00:33:58 -0800

    css int, inline style adj, layout exp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from "./SearchBar"
+import SideList from "./SideList"
+import {render} from 'react-dom';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      eventCategories: []
+      eventCategories: [],
+      events: []
     }
   }
   componentDidMount(){
@@ -22,11 +25,26 @@ class App extends Component {
       console.log(data);
       // this.setState({eventCategories: data});
     });
+    fetch("https://api.meetup.com/find/upcoming_events?photo-host=public&page=20&sig_id=249675255&radius=10&lon=-121.478851&lat=38.575764&sig=7e270d9c33b996ada2a448b9347378e2152f18b0")
+    .then(results=>{
+      if(results.ok){
+        results.json();
+        }else{
+          console.log("didn't get results...");
+        }
+    }).then(data=>{
+      console.log(data);
+      // this.setState({events: data.result});
+    });
   }
 
   render() {
     return (
-      <SearchBar categories = {this.state.eventCategories}/>
+      <div className = 'wrapper'>
+          <SideList className = "sideList" events = {this.state.events}/>
+          <SearchBar className = "searchBar" categories = {this.state.eventCategories}/>
+       </div>
+      
     );
   }
 }
