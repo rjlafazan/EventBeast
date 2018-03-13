7892a719465c37f6f6475d95b369931b3934e1c4
madhavparekh <madhavparekh@gmail.com>
2018-03-12 22:50:57 -0700

    moved weather rendering from GoogleMap.js to here

--- src/Components/InfoDisplay.js
+++ src/Components/InfoDisplay.js
@@ -2,15 +2,37 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class InfoDisplay extends Component {
-    render(){
-        var date = new Date(this.props.event.start)
-        return (  
-            <div>
-                <h2>{this.props.event.name}</h2>
-                <h3>Start time: {date.toLocaleString()}</h3>
-                <div dangerouslySetInnerHTML={{__html: this.props.event.description}}></div>
-                <a href={this.props.event.link}>Learn More</a>
-            </div>
-        )
+  _renderWeather(props) {
+    var weatherSummary = '';
+    console.log(props);
+    console.log(Object.keys(props));
+    if (props.weather) {
+      weatherSummary = `Weather outlook: ${
+        props.weather.summary
+      } Temperature: High - ${props.weather.highTemp} Low - ${
+        props.weather.lowTemp
+      } * Wind: ${props.weather.windSpeed}`;
     }
+
+    return (
+      <div className="weather">
+        <p>{weatherSummary}</p>
+      </div>
+    );
+  }
+
+  render() {
+    var date = new Date(this.props.event.start);
+    return (
+      <div>
+        <h2>{this.props.event.name}</h2>
+        <h3>Start time: {date.toLocaleString()}</h3>
+        <div
+          dangerouslySetInnerHTML={{ __html: this.props.event.description }}
+        />
+        <h6>{this._renderWeather(this.props.event)}</h6>
+        <a href={this.props.event.link}>Learn More</a>
+      </div>
+    );
+  }
 }
